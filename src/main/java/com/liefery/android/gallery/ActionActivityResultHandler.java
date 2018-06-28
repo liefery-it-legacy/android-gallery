71020100c4d1a7b4fd9f2a382291897d9826bb8c
Christian Clampitt <christian.clampitt@nyu.edu>
2018-06-28 14:32:56 +0200

    [#LIEF-9622] Make android-gallery more configurable

---
+++ src/main/java/com/liefery/android/gallery/ActionActivityResultHandler.java
@@ -0,0 +1,53 @@
+package com.liefery.android.gallery;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import java.io.File;
+
+import static com.liefery.android.gallery.GalleryView.EVENT_CANCEL;
+import static com.liefery.android.gallery.GalleryView.EVENT_ERROR;
+import static com.liefery.android.gallery.GalleryView.EVENT_SUCCESS;
+
+public class ActionActivityResultHandler {
+
+    final private Intent data;
+
+    public ActionActivityResultHandler( @Nullable Intent data ) {
+        this.data = data;
+    }
+
+    public int getEvent() {
+        return data.getIntExtra( "event", -1 );
+    }
+
+    @Nullable
+    public Throwable getError() {
+        return (Throwable) data.getSerializableExtra( "error" );
+    }
+
+    @Nullable
+    public File getFile() {
+        String path = data.getStringExtra( "file" );
+
+        if ( path == null ) {
+            // Return null instead of File throwing NullPointerException
+            return null;
+        } else {
+            File file = new File( path );
+            return file;
+        }
+    }
+
+    public boolean isSuccess() {
+        return ( getEvent() == EVENT_SUCCESS );
+    }
+
+    public boolean isCanceled() {
+        return ( getEvent() == EVENT_CANCEL );
+    }
+
+    public boolean isError() {
+        return ( getEvent() == EVENT_ERROR );
+    }
+
+}
