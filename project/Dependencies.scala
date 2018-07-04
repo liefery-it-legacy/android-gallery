a186381a0ce5b49827daf11cec4b913226a1205a
Niklas Klein <mail@taig.io>
2018-07-04 11:19:31 +0200

    Upgrade to photo-view 2.1.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val glide = ( "com.github.bumptech.glide" % "glide" % "4.7.1" )
         .exclude( "com.android.support", "support-annotations" )
 
-    val photoView = ( "com.github.chrisbanes" % "PhotoView" % "2.1.3" )
+    val photoView = ( "com.github.chrisbanes" % "PhotoView" % "2.1.4" )
         .exclude( "com.android.support", "support-core-utils" )
 
     val supportAppcompat = "com.android.support" % "appcompat-v7" % "27.1.1"
